edc49dca4ae0bcb6e13c947cedac105882a37a4b
Joao Juliasz <joaojuliaszdemorais@gmail.com>
2019-12-27 11:30:00 -0300

    usando controle de versao scuba

---
+++ js/func.js
@@ -0,0 +1,73 @@
+
+function hide1() {
+    var div = document.getElementById("hide1");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide2() {
+    var div = document.getElementById("hide2");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide3() {
+    var div = document.getElementById("hide3");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide4() {
+    var div = document.getElementById("hide4");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide5() {
+    var div = document.getElementById("hide5");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide6() {
+    var div = document.getElementById("hide6");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide7() {
+    var div = document.getElementById("hide7");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
+function hide8() {
+    var div = document.getElementById("hide8");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+}
